817de80ac7e8c037791a3388e0bf4ab9e9d4e98a
Abdul9.khan <Abdul9.khan@ril.com>
2024-01-07 20:16:29 +0530

    Change die face

--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,13 +1,91 @@
 import React from "react";
 
-function Die(props) {
+export default function Die(props) {
   const styles = {
-    backgroundColor: props.isHeld ? "#59E391" : "white",
+    backgroundColor: props.isHeld ? "#d63741" : "white",
+  };
+  const dotStyle = {
+    backgroundColor: props.isHeld ? "white" : "#d63741",
+  };
+  const shape = () => {
+    // eslint-disable-next-line default-case
+    switch (props.value) {
+      case 1:
+        return (
+          <div className="dice first-face" style={styles}>
+            <span className="dot" style={dotStyle}></span>
+          </div>
+        );
+      case 2:
+        return (
+          <div className="dice second-face" style={styles}>
+            <span className="dot" style={dotStyle}>
+              {" "}
+            </span>
+            <span className="dot" style={dotStyle}>
+              {" "}
+            </span>
+          </div>
+        );
+      case 3:
+        return (
+          <div className="dice third-face" style={styles}>
+            <span className="dot" style={dotStyle}></span>
+            <span className="dot" style={dotStyle}></span>
+            <span className="dot" style={dotStyle}></span>
+          </div>
+        );
+      case 4:
+        return (
+          <div className="fourth-face dice" style={styles}>
+            <div className="column">
+              <span className="dot" style={dotStyle}></span>
+              <span className="dot" style={dotStyle}></span>
+            </div>
+            <div className="column">
+              <span className="dot" style={dotStyle}></span>
+              <span className="dot" style={dotStyle}></span>
+            </div>
+          </div>
+        );
+      case 6:
+        return (
+          <div className="fourth-face dice" style={styles}>
+            <div className="column">
+              <span className="dot" style={dotStyle}></span>
+              <span className="dot" style={dotStyle}></span>
+              <span className="dot" style={dotStyle}></span>
+            </div>
+            <div className="column">
+              <span className="dot" style={dotStyle}></span>
+              <span className="dot" style={dotStyle}></span>
+              <span className="dot" style={dotStyle}></span>
+            </div>
+          </div>
+        );
+      case 5:
+        return (
+          <div className="fifth-face dice" style={styles}>
+            <div className="column">
+              <span className="dot" style={dotStyle}></span>
+              <span className="dot" style={dotStyle}></span>
+            </div>
+
+            <div className="column">
+              <span className="dot" style={dotStyle}></span>
+            </div>
+
+            <div className="column">
+              <span className="dot" style={dotStyle}></span>
+              <span className="dot" style={dotStyle}></span>
+            </div>
+          </div>
+        );
+    }
   };
   return (
     <div className="die-face" style={styles} onClick={props.holdDice}>
-      <h2 className="die-num">{props.value}</h2>
+      {shape()}
     </div>
   );
 }
-export default Die;
--- src/pages/Tenzies.jsx
+++ src/pages/Tenzies.jsx
@@ -4,13 +4,59 @@ import { nanoid } from "nanoid";
 import { Button, Card, CardActions, CardContent, Grid, Typography } from "@mui/material";
 
 const Tenzies = () => {
-  const [tenzies, setTenzies] = React.useState(false);
-  const [dice, setDice] = React.useState(allNewDice());
-  const diceElements = dice.map((die) => <Die key={die.id} value={die.value} isHeld={die.isHeld} holdDice={() => holdDice(die.id)} />);
+  const [start, setStart] = React.useState(false);
 
-  function generateNewDice() {
+  const [dice, setDice] = React.useState(allNewDice());
+  const [tenzies, setTenzies] = React.useState(false);
+  const [rolls, setRolls] = React.useState(0);
+  const [best, setBest] = React.useState(JSON.parse(localStorage.getItem("best")) || 0);
+  const [bestTime, setBestTime] = React.useState(JSON.parse(localStorage.getItem("time")) || 0);
+
+  const [startTime, setStartTime] = React.useState(null);
+  const [now, setNow] = React.useState(null);
+
+  const intervalRef = React.useRef(null);
+
+  function handleStart() {
+    setStartTime(Date.now());
+    setNow(Date.now());
+
+    clearInterval(intervalRef.current);
+    intervalRef.current = setInterval(() => {
+      setNow(Date.now());
+    }, 10);
+  }
+
+  function handleStop() {
+    clearInterval(intervalRef.current);
+    if (secondsPassed < bestTime || bestTime == 0) {
+      localStorage.setItem("time", secondsPassed);
+      setBestTime(secondsPassed);
+    }
+  }
+
+  let secondsPassed = 0;
+  if (startTime != null && now != null) {
+    secondsPassed = (now - startTime) / 1000;
+  }
+
+  React.useEffect(() => {
+    const allHeld = dice.every((die) => die.isHeld);
+    const firstValue = dice[0].value;
+    const allSameValue = dice.every((die) => die.value === firstValue);
+    if (allHeld && allSameValue) {
+      setTenzies(true);
+      handleStop();
+      if (rolls < best || best === 0) {
+        localStorage.setItem("best", rolls);
+        setBest(rolls);
+      }
+    }
+  }, [dice]);
+
+  function generateNewDie() {
     return {
-      value: Math.trunc(Math.random() * 6) + 1,
+      value: Math.ceil(Math.random() * 6),
       isHeld: false,
       id: nanoid(),
     };
@@ -19,66 +65,96 @@ const Tenzies = () => {
   function allNewDice() {
     const newDice = [];
     for (let i = 0; i < 10; i++) {
-      newDice.push(generateNewDice());
+      newDice.push(generateNewDie());
     }
     return newDice;
   }
 
   function rollDice() {
-    if (!tenzies) {
-      setDice((oldDice) =>
-        oldDice.map((die) => {
-          return die.isHeld ? die : generateNewDice();
-        }),
-      );
+    if (start) {
+      if (!tenzies) {
+        setDice((oldDice) =>
+          oldDice.map((die) => {
+            return die.isHeld ? die : generateNewDie();
+          }),
+        );
+        setRolls(rolls + 1);
+      } else {
+        handleStart();
+        setRolls(0);
+        setTenzies(false);
+        setDice(allNewDice());
+      }
     } else {
-      setTenzies(false);
-      setDice(allNewDice);
+      setRolls(1);
+      setStart(true);
+      handleStart();
     }
   }
 
   function holdDice(id) {
-    setDice((oldDice) =>
-      oldDice.map((die) => {
-        return die.id === id ? { ...die, isHeld: !die.isHeld } : die;
-      }),
-    );
+    if (start) {
+      setDice((oldDice) =>
+        oldDice.map((die) => {
+          return die.id === id ? { ...die, isHeld: !die.isHeld } : die;
+        }),
+      );
+    }
   }
 
-  React.useEffect(
-    function () {
-      const allHeld = dice.every((die) => die.isHeld === true);
-      const firstDice = dice[0].value;
-      const allDiceEqual = dice.every((die) => die.value === firstDice);
-      if (allHeld && allDiceEqual) {
-        setTenzies(true);
-        console.log("You Won!");
-      }
-    },
-    [dice],
-  );
-
+  const diceElements = dice.map((die) => <Die {...die} started={start} holdDice={() => holdDice(die.id)} />);
   return (
-    <Grid container>
-      <Grid item lg={12} md={12} sm={12} xs={12}>
-        <Card sx={{ p: 1 }}>
-          <CardContent>
-            <Typography gutterBottom variant="h6" component="div">
-              🎲Tenzies
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              Roll until all dice are the same. Click each die to freeze it at its current value between rolls.
-            </Typography>
-            <div className="dice-container mt-3">{diceElements}</div>
-          </CardContent>
-          <CardActions>
-            <Button variant="contained" onClick={rollDice}>
-              {tenzies ? "New Game" : "Roll"}
-            </Button>
-          </CardActions>
-        </Card>
+    <>
+      <Grid container>
+        <Grid item lg={12} md={12} sm={12} xs={12}>
+          <Card sx={{ p: 1 }}>
+            <CardContent>
+              <Typography gutterBottom variant="h6" component="div">
+                🎲Tenzies
+              </Typography>
+              <div className="d-flex justify-content-between mb-3">
+                <div className="timer">Current Time: {secondsPassed.toFixed(2)}s</div>
+                {bestTime !== 0 && (
+                  <div>
+                    <Typography>
+                      Best Time:{" "}
+                      <span>
+                        <strong>{bestTime}s</strong>
+                      </span>
+                    </Typography>
+                  </div>
+                )}
+              </div>
+              <Typography variant="body2" color="text.secondary" className="mb-3">
+                Roll until all dice are the same.
+                <br />
+                Click each die to freeze it at its current value between rolls.
+              </Typography>
+              <div className="dice-container mb-5">{diceElements}</div>
+
+              <div className="d-flex justify-content-between">
+                <Typography>
+                  Rolls: <span>{rolls}</span>
+                </Typography>
+                {best !== 0 && (
+                  <Typography>
+                    Best:{" "}
+                    <span>
+                      <strong>{best}</strong>
+                    </span>
+                  </Typography>
+                )}
+              </div>
+            </CardContent>
+            <CardActions>
+              <Button variant="contained" onClick={rollDice}>
+                {!start ? "Start Game" : tenzies ? "New Game" : "Roll"}
+              </Button>
+            </CardActions>
+          </Card>
+        </Grid>
       </Grid>
-    </Grid>
+    </>
   );
 };
 
